dd01a1e6ac24c2b302858804b77f5cbc5eed0a82
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-01 00:21:08 +0530

    JavaScript variables
    JavaScript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,14 @@
+var cityName = "Pune";
+console.log(`My City Name Is: ${cityName}`);
+
+var bestFriend = "Balasaheb";
+console.log(`My Best Friend Name Is: ${bestFriend}`);
+
+var countryName = "India";
+console.log(`My Country Name Is: ${countryName}`);
+
+var schoolName = "Abinav Vidya Vihar High School Purna, Parbhani";
+console.log(`My School Name Is: ${schoolName}`);
+
+var clgName = "Gov.Collage Of Engineering Chandrapur";
+console.log(`My Collage Name Is: ${clgName}`);
---
+++ 01-variable.js
@@ -0,0 +1,15 @@
+//  Variable Declaration
+var firstName;
+console.log(firstName);
+
+// Variable Initialization
+firstName = "Balasaheb";
+console.log(firstName);
+
+//  Variable Declaration &  Initialization
+var lastName = "Bhad";
+console.log(lastName);
+
+//  Variable Updates
+firstName = "Bala";
+console.log(firstName);
