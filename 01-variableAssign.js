5cc77a234af0d1bfa7cb07d8324d47f946a816f9
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-01 22:55:12 +0530

    Assignment on Variable, Datatypes and typeof operator
    Assignment on Variable, Datatypes and typeof operator

---
+++ 01-variableAssign.js
@@ -0,0 +1,48 @@
+console.log('========= 01 ============');
+var myFullName;
+console.log(`My Full Name Before Initialization: ${myFullName}`);
+
+var myFullName = "Ashok Bhad";
+console.log(`My First & Last Name After Initialization: ${myFullName}`);
+
+var myFullName = "Ashok Damodhar Bhad";
+console.log(`My Full Name After Update: ${myFullName}`);
+
+console.log('========= 02 ==========');
+var myLove = "I Love Only JS";
+console.log(`${myLove}`);
+
+console.log('========= 03 ==========');
+var firstName = "Ashok";
+console.log(`My First Name is: ${firstName}`);
+
+var lastName = "Bhad";
+console.log(`My Last Name is: ${lastName}`);
+
+var age = "24";
+console.log(`My Age is: ${age}`);
+
+var address = "Marunji Village, Mulshi, Pune,";
+console.log(`My Address is: ${address}`);
+
+var pinCode ="411057";
+console.log(`My Area Pincode is: ${pinCode}`);
+
+var gender ="Male";
+console.log(`My Gender is: ${gender}`);
+
+var isMarried = "false";
+console.log(`I Am Married: ${isMarried}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02-typeOf.js
@@ -0,0 +1,33 @@
+console.log("=== variable, Datatype & typeof operator ===");
+
+console.log("=========== 01 ============");
+var bankName = "State Bank Of India";
+console.log(`I Have Bank Account in: ${bankName}`);
+var bankNameTypeOf = typeof bankName;
+console.log(`Datatype of Bank is: ${bankNameTypeOf}`);
+
+console.log("=========== 02 ============");
+var marks = "90";
+var marksTypeOf = typeof marks;
+console.log(`My Obtained is: ${marks} And Datatype of the marks is:${marksTypeOf}`);
+
+console.log("=========== 03 ============");
+var isWorking;
+console.log(`Value of isWorking Before Initialization is: ${isWorking}`);
+var isWorkingTypeOf = typeof isWorking;
+console.log(`Datatype of isWorking Before Initialization is: ${isWorkingTypeOf}`);
+
+var isWorking = true;
+console.log(`Value of isWorking After Initialization is: ${isWorking}`);
+var isWorkingTypeOf = typeof isWorking;
+console.log(`Datatype of isWorking After Initialization is:${isWorkingTypeOf}`);
+
+console.log('=========== 04 ==============');
+var totalCount = 'Hundred and Seven';
+var totalCountTypeOf = typeof totalCount;
+console.log(`Value of the Total Count is: ${totalCount} & Datatype of total Count is: ${totalCountTypeOf}`);
+
+var totalCount = 107;
+var totalCountTypeOf = typeof totalCount;
+console.log(`Value of the Total Count After Update is: ${totalCount} & Datatype of total Count is: ${totalCountTypeOf}`);
+
