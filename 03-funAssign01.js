d4aeb5ff258648791650b53a4322745c7684b1d4
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-02 22:58:33 +0530

    more assign on normal functions
    more assign on normal functions

---
+++ 03-funAssign01.js
@@ -0,0 +1,38 @@
+console.log(`========== Task No.1 =========`);
+function jsDemo() {
+  console.log("JavaScript Learning Started");
+}
+jsDemo();
+
+function jsDemo2() {
+  console.log("This is a JavaScript Function");
+}
+jsDemo2();
+
+console.log(`========== Task No.2 =========`);
+function personalDetails() {
+  }
+ var returnvalue = 'Ashok';
+ console.log(`My First Name is: ${returnvalue}`);
+
+ var returnvalue = 'Bhad';
+ console.log(`My Last Name is: ${returnvalue}`);
+
+ var returnvalue = 'GCOEC';
+ console.log(`My College Name is: ${returnvalue}`);
+
+ console.log('=========== Task 03 ==========');
+ function addThreeValue(num1, num2, num3){
+    var result = num1 + num2 + num3;
+    return result;
+}
+var returnValue = (10.23 + 600 + 40);
+console.log("Addition of first three value is: ", returnValue);
+
+var returnValue = ('Hello' + ' Good' + ' Morning');
+console.log("Addition of Second three value is: ", returnValue);
+ 
+ 
+
+
+  
---
+++ 03-funAssign02.js
@@ -0,0 +1,13 @@
+console.log('=========== Bank Details =============');
+
+function bankDetails() {
+}
+var returnValue = ('Bank Name is: CITI Bank,' + ' A/C: 2343756745843,' + ' Branch: Pune,' + ' PinCode: 411033');
+console.log("Primary Account Details: ", returnValue);
+
+var returnValue = ('Bank Name is: Axis bank,' + ' A/C: 6577676734333,' + ' Branch: Mumbai,' + ' PinCode: 763772');
+console.log("Second Account Details: ", returnValue);
+
+var returnValue = ('Bank Name is: HDFC Bank,' + ' A/C: 9574546548945,' + ' Branch: Delhi,' + ' PinCode: 655533');
+console.log("Third Account Details: ", returnValue);
+
