c89f48000f188a0498f298e3b8778595cf97cafa
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-05 12:24:18 +0530

    operators
     operators

---
+++ 04-operator-comparision.js
@@ -0,0 +1,23 @@
+console.log("%c*** Comparison Operators ***","color: purple");
+
+var n1 = "50";
+var n2 = 50;
+
+//var result = n1 == n2
+console.log(n1 == n2);
+console.log(n1 === n2);
+
+var n1 = 10;
+var n2 = 20;
+console.log(n1!=n2);
+console.log(n1>n2);
+console.log(n1>10);
+console.log(n1>=10);
+
+console.log(n1<n2);
+console.log(n1<10);
+console.log(n1<=10);
+
+
+
+
