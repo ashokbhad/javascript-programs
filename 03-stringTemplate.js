708824a015a89d224dddd6d7ff36689575c020b3
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-05 12:23:54 +0530

    string template and operators
    string template and operators

---
+++ 03-stringTemplate.js
@@ -0,0 +1,5 @@
+console.log("%c============= String Template ==========", "color: purple");
+console.log(`I love only "JavaScript"`);
+var city = "Pune";
+var country = "India";
+console.log(`My city name is: ${city} and Country is: ${country}`);
