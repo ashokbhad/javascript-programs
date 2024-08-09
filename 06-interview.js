4b9b44ce9ca8fc37473e357917c5f275920ac918
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-09 13:36:11 +0530

    interview eligibility check with if-else statement
    interview eligibility check with if-else statement

---
+++ 06-interview.js
@@ -0,0 +1,12 @@
+console.log(`%c*** Interview Eligibility Check with if-else statement ***`, "color: purple");
+
+function interviewCheck(gradScore, hscScore, sscScore, candidateName) {
+  const result =
+    gradScore >= 70 || hscScore >= 80 || sscScore > 90
+      ? `Congrats "${candidateName}", you are eligible for Google interview.`
+      : `Unfortunately "${candidateName}", you are not eligible for Google interview.`;
+  console.log(result);
+}
+interviewCheck(82, 86, 90, "Rohit");
+interviewCheck(70, 66, 55, "Virat");
+interviewCheck(60, 79, 88, "Rishabh");
