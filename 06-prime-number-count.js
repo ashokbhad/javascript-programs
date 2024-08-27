79c6b035940213a5295be8ccdf874a7beb0ad81b
ashok-bhad <160321755+ashokbhad@users.noreply.github.com>
2024-08-27 21:44:02 +0530

    prime numbers count
    prime numbers count

--- 06-prime-number-count.js
+++ 06-prime-number-count.js
@@ -89,3 +89,31 @@ duplicateCharCount("Apple");
 
 console.log("%c>>> Duplicate characters in 'Software Developer' <<<", "color: yellow");
 duplicateCharCount("Software Developer");
+
+
+
+
+console.log(`reference for assign B from class`);
+
+let str = "Banana";  // a - 3, n - 2
+let finalOutput = {};
+for (let index = 0; index < str.length; index++) {
+    const element = str.charAt(index);
+    let count = 0;
+    for (let j = 0; j < str.length; j++) {
+        const char = str.charAt(j);
+        if (char == element) {
+            count++;
+        }
+    }
+    // console.log(element, count);
+    if (count > 1) {
+        finalOutput[element] = count;
+    }
+}
+// console.log(finalOutput);
+for (const key in finalOutput) {
+    const element = finalOutput[key];
+    console.log(`${key} - ${element}`);
+
+}
